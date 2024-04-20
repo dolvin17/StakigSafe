e86557c2356621861e98c3b76796b014bf6d16ac
dolvin17 <huertas876@gmail.com>
2024-04-20 15:01:43 +0200

    adding portfolio component to main pge

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+import Navigation from "@/components/navigation/navigation";
+
+export default function Portfolio() {
+  return (
+	<>
+	<Navigation />
+    <div className="bg-black m-1 p-4 sm:mx-auto sm:w-1/2 py-8">
+      <div className=" text-white flex flex-row justify-between">
+        <div>
+          <div className="text-3xl font-black ">$1260</div>
+          <div className="bg-[#adff02] p-1 rounded-sm text-xs text-black font-black ">
+            +$33.38 (10.4%)
+          </div>
+        </div>
+      </div>
+      <Link href="/">
+        <div className="bg-[#adff02] font-black text-xl p-4 mt-8 rounded-lg text-center hover:bg-lime-200 hover:text-black">
+          Deposit {"&"} Stake
+        </div>
+      </Link>
+    </div>
+	</>
+  );
+}
