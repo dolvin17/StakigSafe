64dddde306e0f6a63f364deaeadc9f377426aaef
dolvin17 <huertas876@gmail.com>
2024-04-20 05:54:00 +0200

    navbar incommmming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Button } from "@/components/ui/button"
 import Transfer from '@/components/home/transfer';
 import History from '@/components/home/history';
+import Navigation from '@/components/navigation/navigation';
 
 export default function Page() {
 	return (
 		<>
+		<Navigation />
 		<Transfer />
 		<History />
 		</>
