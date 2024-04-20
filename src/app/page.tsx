6aef81af34fa94ab9c3bee502b1d666d68528068
dolvin17 <huertas876@gmail.com>
2024-04-20 02:15:44 +0200

    adding sets and shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+'use server'
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [GitHub],
+})
