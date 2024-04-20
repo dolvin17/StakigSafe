93ff0bf84a30167096783634ee69aae8a7fadf16
dolvin17 <huertas876@gmail.com>
2024-04-20 03:54:06 +0200

    incrusting module history

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Button } from "@/components/ui/button"
 import Transfer from '@/components/home/transfer';
+import History from '@/components/home/history';
+
 export default function Page() {
 	return (
 		<>
 		<Transfer />
+		<History />
 		</>
 	)
 }
