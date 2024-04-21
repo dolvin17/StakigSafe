9aae9e7c522b272a6b46732c4fa02c6fab8e0757
dolvin17 <huertas876@gmail.com>
2024-04-21 16:46:01 +0200

    connecting ui, wagmi +rb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,62 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import Link from "next/link";
+import { Rkbutton } from "@/components/rainbowkit/rkbutton";
+import {
+  useConnectModal,
+  useAccountModal,
+  useChainModal,
+} from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const { address } = useAccount();
+  const { openChainModal } = useChainModal();
+  const { openConnectModal = openChainModal } = useConnectModal();
+  const { openAccountModal = openConnectModal } = useAccountModal();
+  const router = useRouter();
+
+  useEffect(() => {
+	if (address) {
+	  router.push("/welcome");
+	}
+  }, [address]);	
   return (
     <>
-	<div>
-		<Image className="absolute hover:scale-125 animate-pulse animate-infinite animate-duration-[10000ms] " alt="melon" src="/fam23(1).png" width={150} height={150} />
-	</div>
-	<div className="w/1 flex flex-col items-center h-[900px] justify-center">
-
-      <div className="   flex scale-125 items-center justify-center ">
-        <Image className="animate-pulse animate-infinite animate-duration-[3000ms] animate-" alt="jungle" src="/jungle.svg" width={700} height={500} />
+      <div>
+        <Image
+          className="absolute hover:scale-125 animate-pulse animate-infinite animate-duration-[10000ms] "
+          alt="melon"
+          src="/fam23(1).png"
+          width={150}
+          height={150}
+        />
       </div>
-		<Image className="absolute hover:scale-125 animate-pulse animate-infinite animate-duration-[10000ms] animate-ease-out translate-x-8 -translate-y-24" alt="melon" src="/melon.png" width={200} height={200} />
-	  <div className=" flex gap-12   flex-col text-white w-1/2 items center justify-center">
-	  </div>
-
-	<Link href="/welcome">
-	  <div className="text-[#adff02] hover:scale-125 animate-pulse animate-infinite animate-duration-[10000ms m-4 w-[200px] text-center border-[#adff02] rounded-xl border-2 border-double bg-black p-4"> Connec a Wallet</div>
-	</Link>
-	  <div className="text-[#adff02] hover:scale-125 animate-pulse animate-infinite animate-duration-[10000ms m-4 w-[200px] text-center border-[#adff02] rounded-xl border-2 border-double bg-black p-4"> Create Safe Account</div>
-
-	</div>
-	
-
+      <div className="w/1 flex flex-col items-center h-[900px] justify-center">
+        <div className="   flex scale-125 items-center justify-center ">
+          <Image
+            className=" pointer-events-none animate-pulse animate-infinite animate-duration-[3000ms] animate-"
+            alt="jungle"
+            src="/jungle.svg"
+            width={700}
+            height={500}
+          />
+        </div>
+        <Image
+          className=" absolute hover:scale-125 animate-pulse animate-infinite animate-duration-[10000ms] animate-ease-out translate-x-8 -translate-y-24"
+          alt="melon"
+          src="/melon.png"
+          width={200}
+          height={200}
+        />
+        <div className="text-[#adff02] hover:scale-125 animate-pulse animate-infinite animate-duration-[10000ms m-4 w-[200px] text-center border-[#adff02] rounded-xl border-2 border-double bg-black p-4">
+          {" "}
+          Create Safe Account
+        </div>
+        <Rkbutton />{" "}
+      </div>
     </>
   );
 }
--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,26 +1,26 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import { Rkbutton } from "../rainbowkit/rkbutton";
 
 export default function Navigation() {
   return (
     <>
       <div className=" px-2 flex bg-transparent backdrop-blur-sm flex-row gap-16 sm:w-1/2 mx-auto py-8">
-		<Link href="/">
-
-        <Image alt="sol" src="/fam23.png" width={150} height={50} />
-		</Link>
+        <Link href="/">
+          <Image alt="sol" src="/fam23.png" width={150} height={50} />
+        </Link>
         <div className="items-center">
-          <Link href="/">
-            <div className="text-[#adff02] m-4 w-[100px] hover:border-black hover:bg-white hover:text-black text-center border-[#adff02] rounded-xl border-2 border-double bg-black p-4">
+          <Link href="/permitionsless">
+            <div className="text-[#adff02] hover:scale-125  m-2 w-[200px] text-center border-[#adff02] rounded-xl border-2 border-double bg-black p-4 ">
               Home
             </div>
           </Link>
           <Link href="/portfolio">
-            <div className="text-[#adff02] m-4  w-[100px]  hover:border-black hover:bg-white hover:text-black text-center border-[#adff02] rounded-xl border-2 border-double bg-black p-4 ">
+            <div className="text-[#adff02] hover:scale-125  m-2 w-[200px] text-center border-[#adff02] rounded-xl border-2 border-double bg-black p-4 ">
               Portfolio
             </div>
           </Link>
+          <Rkbutton />
         </div>
       </div>
     </>
